196a5d87e1c31620a1bd8813687ca505353b10e8
Binita-Sharma <binitasharma644@gmail.com>
2025-07-06 16:35:37 +0530

    clear the error in postman

--- Backend/controller/message.controller.js
+++ Backend/controller/message.controller.js
@@ -1,3 +1,59 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
 export const sendMessage = async (req, res) => {
-    console.log("Message sent to user ");
+  //console.log("Message sent to binita mam",req.params.id, req.body.message);
+
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id; // Assuming req.user is populated with the authenticated user's info , basically current logeed in user
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+    if (!conversation) {
+      // If conversation doesn't exist, create a new one
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+      const newMessage = new Message({
+        senderId,
+        receiverId,
+        message,
+      });
+
+      if (newMessage) {
+        conversation.messages.push(newMessage._id);
+      }
+      await Promise.all([conversation.save(), newMessage.save()]);
+      res.status(201).json({
+        message: "Message sent successfully", newMessage
+      });
+    
+  } catch (error) {
+    console.error("Error sending message:" + error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const { id: chatUser } = req.params;
+    const senderId = req.user._id; // Assuming req.user is populated with the authenticated user's info
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, chatUser] },
+    }).populate("messages");//for actual message data
+
+    if (!conversation) {
+      return res.status(201).json([]);
+    }
+    const messages = conversation.messages;
+    res.status(201).json(messages);
+  } catch (error) {
+    console.error("Error fetching messages:" + error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
